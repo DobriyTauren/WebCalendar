138063b34d94f7785136f59f5c84e9869b80dc63
dobriytauren <dobriytauren@gmail.com>
2025-08-26 17:53:29 +0300

    Добавьте файлы проекта.

---
+++ Code/CalendarDay.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Drawing;
+
+namespace WebCalendar.Client.Code
+{
+    public class CalendarDay
+    {
+        public DateTime Date { get; set; }
+        public Color Color { get; set; } = Color.White;
+        public bool IsMarked { get; set; }
+        public ElementReference ColorInputRef { get; set; }
+    }
+}
---
+++ tailwind-frontend/vite.config.js
@@ -0,0 +1,6 @@
+﻿export default {
+    build: {
+        outDir: '../wwwroot/css',
+        emptyOutDir: true,
+    }
+}
